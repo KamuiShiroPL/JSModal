d6bd52d9961f3401b3e7f61e38888bf60bf409f3
Pawel <paweldudek1988@gmail.com>
2018-10-10 18:09:23 +0900

    Modify Modal. Add open, close, toggle functionality

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-function openModal() {
-    var modal = document.querySelector('.modal');
+//Modal query slectors
 
-    modal.style.display = "block";
+modal = document.querySelector('.modal');
+modalTitle = document.querySelector('.modal-title');
+modalContent = document.querySelector('.modal-content');
+
+//Open Modal
+function openModal(modal_title, modal_content) {
+    modalTitle.innerHTML = modal_title + ' <button type="button" button onclick="closeModal();"> X </button>';
+    modalContent.innerHTML = modal_content;
+
+    if(modal.style.display == 'block') {
+        //Modal off
+        closeModal();
+    } else {
+        //Modal on
+        modal.style.display = 'block';
+    }
 }
+//Close modal 
+function closeModal() {
+    modal.style.display = 'none';
+
+    //modal data reset
+    modalTitle.innerHTML = '';
+    modalContent.innerHTML = '';
+}
+
+
+
+
+
+
+
+
